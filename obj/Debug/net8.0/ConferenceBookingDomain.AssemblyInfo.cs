0a134a44d11824187bebfbc5c897b16974164670
Masoka Andile Mohono <masokandile17@gmail.com>
2026-01-28 03:33:48 +0200

    Update AssemblyInfo with new informational version and cache input

--- obj/Debug/net8.0/ConferenceBookingDomain.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConferenceBookingDomain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9897c3cce5d951715432164ff3ce9174869171f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b9c62c0f57351c178d962843a0fa6b0827b196")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConferenceBookingDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
