8499577de45a7de61132ebe77157f2b0047f7a77
Masoka Andile Mohono <masokaandile17@gmail.com>
2026-02-09 09:15:51 +0200

    commit to clone my repo

--- Bookinglib/obj/Debug/net8.0/Bookinglib.AssemblyInfo.cs
+++ Bookinglib/obj/Debug/net8.0/Bookinglib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7266abf756a140b979e23142e21d12b51ab1c935")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef16eeb1c4fa9f75c897e9daec8b5de3c190aa55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
