a22f22dd4a2a64fd24a10778c0dbd5034ed9226f
Masoka Andile Mohono <masokandile17@gmail.com>
2026-02-11 23:46:19 +0200

    commit to allow me to work from home

--- Bookinglib/obj/Debug/net8.0/Bookinglib.AssemblyInfo.cs
+++ Bookinglib/obj/Debug/net8.0/Bookinglib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1069dd88363028e72de5558bdecccad21fe11c8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e44a94c815125f5ecfec7f7a9a7519c3990811")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
