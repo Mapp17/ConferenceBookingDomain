5fb0101649e55a30583b72d1fc6a5275aee3803a
Masoka Andile Mohono <masokaandile17@gmail.com>
2026-02-12 10:55:44 +0200

    Update AssemblyInfo attributes and cache file for version consistency

--- Bookinglib/obj/Debug/net8.0/Bookinglib.AssemblyInfo.cs
+++ Bookinglib/obj/Debug/net8.0/Bookinglib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f4016226da78c610cb045e0642431044c7c505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+948d5a787e4b820e70ca8cdca44ddbc1a24de698")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
