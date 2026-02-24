77d20f37a1da16de0f04087eea1fe651bf18c3e8
Masoka Andile Mohono <masokandile17@gmail.com>
2026-02-25 00:19:57 +0200

    commit to create a new branch

--- Bookinglib/obj/Debug/net8.0/Bookinglib.AssemblyInfo.cs
+++ Bookinglib/obj/Debug/net8.0/Bookinglib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd44d1a6e6cacc4229264a6725919c10c4df347d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b184d1855ec87668aea2fe0fdd471068f16abf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookinglib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
